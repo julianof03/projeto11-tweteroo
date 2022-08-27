243ebab6b2c9dc359939dba975058879ebbdbcb6
julianof03 <jlprod08@gmail.com>
2022-08-27 15:23:47 -0300

    Impelentação Get e Post

--- index.js
+++ index.js
@@ -3,10 +3,49 @@ import cors from 'cors'
 
 const server = express();
 server.use(cors());
+server.use(express.json());
+const corpo = [];
+let usereq;
+let avatareq;
+const tweetCorpo = [];
+let tweetuser;
+let tweet;
 
-server.get('/sign-up', function (request, response){
-    console.log('chegou aqui');
-    response.send('sai daqui');
-})
+server.post('/sign-up', (req, res) =>{
+    usereq = req.body.username;
+    avatareq = req.body.avatar;
+    const novoCorpo = {
+        username: avatareq,
+        avatar: avatareq
+    };
 
-server.listen(5000)
+    corpo.push(novoCorpo);
+    res.send("OK");
+  });
+
+  server.post('/tweets', (req, res) =>{
+    tweetuser = req.body.username;
+    tweetreq = req.body.tweet;
+    const novotweetCorpo = {
+      username: tweetuser,
+      tweet: tweet
+    };
+
+    tweetCorpo.push(novotweetCorpo);
+    res.send("OK");
+  });
+  server.get('/tweets', (req, res) =>{
+    tweetuser = req.body.username;
+    tweetreq = req.body.tweet;
+    const novotweetCorpo = {
+      username: tweetuser,
+      avatar: avatareq,
+      tweet: tweetreq
+    };
+
+    tweetCorpo.push(novotweetCorpo);
+    res.send(tweetCorpo);
+  });
+
+
+server.listen(5000, () => console.log('Listening on port 5000'));
